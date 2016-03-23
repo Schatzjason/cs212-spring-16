f9e4e3e537061c736295ba85213ca46f559be3f6
schatzjason <schatz@gmail.com>
2016-03-22 18:45:55 -0700

    Glaxy

---
+++ Galaxy/Galaxy/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Galaxy
+//
+//  Created by ccsfcomputers on 3/22/16.
+//  Copyright © 2016 ccsfcomputers. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+     let GalaxyURL: String = "https://s3.amazonaws.com/policymic-images/oidbjjmfalmyrfd1ohj8sjezcqu7kwxg7bpil5pddk8ky09amyszdhenheurzrj2.jpg"
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
