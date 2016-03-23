1168166dc96723dfc180ef9afa4accfe44e559ce
schatzjason <schatz@gmail.com>
2016-03-22 19:23:25 -0700

    Galaxy 2

--- Galaxy/Galaxy/ViewController.swift
+++ Galaxy/Galaxy/ViewController.swift
@@ -13,9 +13,49 @@ class ViewController: UIViewController {
      let GalaxyURL: String = "https://s3.amazonaws.com/policymic-images/oidbjjmfalmyrfd1ohj8sjezcqu7kwxg7bpil5pddk8ky09amyszdhenheurzrj2.jpg"
     
     @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
     }
+    
+    @IBAction func startDowload(sender: UIButton) {
+        downloadGalaxy()
+    }
+    
+    func downloadGalaxy() {
+    
+        // 1. URL - done
+        
+        let url = NSURL(string: GalaxyURL)!
+        
+        // 2. Create Task that can get our data
+        
+        let task = NSURLSession.sharedSession().dataTaskWithURL(url)
+        {
+            
+            data, response, error in
+            
+            assert(error == nil, error!.description)
+            
+            if let data = data {
+                // 3. Turn data into image
+                let image = UIImage(data: data)
+            
+                // 4. Update UI
+                dispatch_async(dispatch_get_main_queue()) {
+                    self.imageView.image = image
+                }
+            }
+        }
+        
+        // Start the task working....
+        task.resume()
+        
+
+    }
 }
+
+
+
 
